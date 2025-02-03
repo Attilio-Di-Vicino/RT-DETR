2718e087abcb2e5a3e7764e24398801d6c1d3d7a
Attilio Di Vicino <attiliodivicino@libero.it>
2025-02-03 12:32:11 +0100

    test

--- rtdetr_pytorch/dataset2/image_list_creator.py
+++ rtdetr_pytorch/dataset2/image_list_creator.py
@@ -1,7 +1,8 @@
 import csv
 
-csv_file = 'Open Images V7 Extended Miap Boxes Test.csv'  
-txt_file = 'image_list.txt'
+csv_file = 'Open Images V7 Extended Miap Boxes.csv' 
+split = "validation"
+txt_file = f'image_list_{split}.txt'
 
 with open(csv_file, 'r') as csvfile:
     reader = csv.reader(csvfile)
@@ -10,6 +11,6 @@ with open(csv_file, 'r') as csvfile:
     with open(txt_file, 'w') as f:
         for row in reader:
             image_id = row[0]
-            f.write(f'test/{image_id}\n')
+            f.write(f'{split}/{image_id}\n')
 
 print(f'File {txt_file} creato con successo!')
